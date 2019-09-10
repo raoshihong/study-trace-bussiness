fad16976762ae0cb1f75476ad4fdc630bbe8f6d5
honey.rao <honey.rao@wolaidai.com>
2019-09-10 19:05:22 +0800

    Initial commit

---
+++ study-trace-bussiness-core/src/main/java/com/rao/study/trace/bussiness/mapper/UserLoginMapper.java
@@ -0,0 +1,16 @@
+package com.rao.study.trace.bussiness.mapper;
+
+import com.rao.study.trace.bussiness.entity.UserLogin;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 
+ * @since 2019-09-10
+ */
+public interface UserLoginMapper extends BaseMapper<UserLogin> {
+
+}
---
+++ study-trace-bussiness-core/src/main/java/com/rao/study/trace/bussiness/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.rao.study.trace.bussiness.mapper;
+
+import com.rao.study.trace.bussiness.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 
+ * @since 2019-09-10
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
